537733f6ee507290a1c358dfbc17e157fe5a7845
Ligia Azevedo <145520846+ligiahelenaan@users.noreply.github.com>
2024-06-12 02:59:55 -0400

    PA1

--- demo/components/HomeScreen.js
+++ demo/components/HomeScreen.js
@@ -4,7 +4,6 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
 
 function HomeScreen({ navigation }) {
 
